724a6bbcdaeef0cfbaea6358b6696b6c3c729fac
emma-landry7 <emmablandry730@gmail.com>
2024-01-17 02:15:29 -0600

    started lab 1 of Module 7

---
+++ labs/components/BitcoinRates.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const currencies = ['USD', 'AUD', 'NZD', 'GBP', 'EUR', 'SGD'];
+
+function BitcoinRates() {
+    const [currency, setCurrency] = useState(currencies[0]);
+    const options = currencies.map(curr => <option value={curr} key={curr}>{curr}</option>);
+    const [price, setPrice] = useState('')
+
+    useEffect(() => {
+        let ignore = false
+        fetch(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}`)
+            .then(response => response.json())
+            .then(json => {
+                setCurrency(currency)
+                let curr = json.bitcoin
+                console.log(price)
+            })
+        return () => {
+            ignore = true
+        }
+    }, [])
+
+    return (
+        <div>
+            <h3>Bitcoin Exchange Rate</h3>
+            <label>Choose currency:
+                <select value={currency} onChange={e => setCurrency(e.target.value)}>
+                    {options}
+                </select>
+            </label>
+            <p>Price: ${price}</p>
+        </div>
+    )
+}
+
+export default BitcoinRates;
--- labs/src/App.jsx
+++ labs/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BitcoinRates from '../components/BitcoinRates'
 
 function App() {
 
   return (
     <>
       <h1>Module 7 Labs</h1>
+      <BitcoinRates />
     </>
   )
 }
