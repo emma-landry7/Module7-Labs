06a8322d6efb5aa52318b34579eeafb8f94e46ae
emma-landry7 <emmablandry730@gmail.com>
2024-01-27 10:29:01 -0600

    working on lab 4 of Module 7

--- labs/src/pages/HomePage.jsx
+++ labs/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import BitcoinRates from "../components/BitcoinRates";
+import Emoji from "../components/Emoji";
 
 export default function HomePage() {
     return (
         <div>
             <h1>Home</h1>
             <BitcoinRates />
+            <Emoji />
         </div>
     )
 }
